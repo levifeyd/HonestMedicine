b0da72f3d303ad71765c73c91b09699dd79c02e9
levifeyd <kuleshov-as@mail.ru>
2023-08-11 14:52:44 +0400

    require bootstrap

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ItemController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/', [ItemController::class, 'index'])->name('index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
